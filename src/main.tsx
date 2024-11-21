ceea4d40bd249900239eca84e52b6827f5d48b2b
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-11-21 00:33:25 +0000

    feat: updaing the test project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Footer } from '../lib/components/root/footer';
+import { Header } from '../lib/components/root/header';
+import { Page } from '../lib/components/root/main/page';
+import { PageSection } from '../lib/components/root/main/page-section';
+import './style.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<Header>
+			<h1>This is a header</h1>
+		</Header>
+		<Page>
+			<PageSection>
+				<h1>This is the main section</h1>
+			</PageSection>
+		</Page>
+		<Footer>
+			<p>This is a footer</p>
+		</Footer>
+	</StrictMode>
+);
