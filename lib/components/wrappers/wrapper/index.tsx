d2b89656f2b3343e5b7e38d4e00f1612e587087e
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-11-21 23:15:20 +0000

    feat: adding a new wrapper component

---
+++ lib/components/wrappers/wrapper/index.tsx
@@ -0,0 +1,26 @@
+import type { HTMLAttributes } from 'react';
+import { Combine } from '../../../functions/combine';
+import styles from './styles.module.css';
+
+interface Properties extends HTMLAttributes<HTMLDivElement> {
+	fullWidth?: boolean;
+}
+
+export function Wrapper(properties: Properties) {
+	const { fullWidth, children, className, ...otherProperties } = properties;
+
+	return (
+		<div
+			className={Combine([
+				'standard-component',
+				'wrapper',
+				fullWidth ? 'full' : '',
+				styles[fullWidth ? 'wrapper-full' : 'wrapper'],
+				className,
+			])}
+			{...otherProperties}
+		>
+			{children}
+		</div>
+	);
+}
