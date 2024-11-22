d3c3bac7fdc53cb1defacfb3898fc4c7db2bfc87
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-11-22 23:59:08 +0000

    feat: adding button components to the test page

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,5 @@
+export { Button } from './components/buttons/button';
+export { RadioButton } from './components/buttons/radio-button';
 export { Footer } from './components/root/footer';
 export { Header } from './components/root/header';
 export { Page } from './components/root/main/page';
