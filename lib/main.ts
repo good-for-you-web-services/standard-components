6f38cd72287b46f9eb0efb2f8dedc798af340ba8
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-11-23 00:26:36 +0000

    feat: adding an anchor button component

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,6 @@
+export { AnchorButton } from './components/buttons/anchor-button';
 export { Button } from './components/buttons/button';
+export { ButtonGroup } from './components/buttons/button-group';
 export { RadioButton } from './components/buttons/radio-button';
 export { Footer } from './components/root/footer';
 export { Header } from './components/root/header';
