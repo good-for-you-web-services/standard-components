c9d0bb99a7c3008747b64e7514be81e93997c7c6
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-11-27 21:39:41 +0000

    feat: adding the navigation component to the test page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import Naviation from '../lib/components/misc/navigation';
 import { Footer } from '../lib/components/root/footer';
 import { Header } from '../lib/components/root/header';
 import Test from './pages/test';
@@ -9,6 +10,12 @@ createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Header>
 			<h1>This is a header</h1>
+			<Naviation>
+				<a href='/'>Page 1</a>
+				<a href='/'>Page 2</a>
+				<a href='/'>Page 3</a>
+				<a href='/'>Page 4</a>
+			</Naviation>
 		</Header>
 		<Test />
 		<Footer>
