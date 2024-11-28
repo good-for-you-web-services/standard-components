641b1978ddbe1de4cfb2f77ed4f077034d80cfa2
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-11-28 00:41:52 +0000

    fix: removing responsive breakpoint from the navigation css module, and into the sample project

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,13 @@ import { Footer } from '../lib/components/root/footer';
 import { Header } from '../lib/components/root/header';
 import Test from './pages/test';
 import './style.css';
+import styles from './styles.module.scss';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Header>
 			<h1>This is a header</h1>
-			<Naviation>
+			<Naviation className={styles['navigation']}>
 				<a href='/'>Page 1</a>
 				<a href='/'>Page 2</a>
 				<a href='/'>Page 3</a>
