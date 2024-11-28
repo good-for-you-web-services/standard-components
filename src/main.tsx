f1cdcd57fe2bde5e993d3f0c3d493c45a3365c53
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-11-28 01:48:09 +0000

    feat: removing previous method of responsive styling from the test page

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Naviation from '../lib/components/misc/navigation';
+import Navigation from '../lib/components/misc/navigation';
 import { Footer } from '../lib/components/root/footer';
 import { Header } from '../lib/components/root/header';
 import Test from './pages/test';
 import './style.css';
-import styles from './styles.module.scss';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Header>
 			<h1>This is a header</h1>
-			<Naviation className={styles['navigation']}>
+			<Navigation>
 				<a href='/'>Page 1</a>
 				<a href='/'>Page 2</a>
 				<a href='/'>Page 3</a>
 				<a href='/'>Page 4</a>
-			</Naviation>
+			</Navigation>
 		</Header>
 		<Test />
 		<Footer>
