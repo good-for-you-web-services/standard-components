35b8b8a27ed268fde17200fbe478dbaa9b7809b8
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-12-02 22:51:11 +0000

    feat: adding a hero section component

---
+++ lib/components/sections/hero-section/index.tsx
@@ -0,0 +1,39 @@
+import { useMediaQuery } from '@uidotdev/usehooks';
+import { type HTMLAttributes, type ReactElement } from 'react';
+import { Combine } from '../../../functions/combine';
+import { Wrapper } from '../../wrappers/wrapper';
+import styles from './styles.module.scss';
+
+interface Properties extends HTMLAttributes<HTMLBaseElement> {
+	heading?: ReactElement;
+	wideLayoutBreakpoint?: string;
+}
+
+export function HeroSection({
+	children,
+	className,
+	heading,
+	wideLayoutBreakpoint = '1024px',
+	...properties
+}: Properties) {
+	const isWideLayout = useMediaQuery(`only screen and (min-width: ${wideLayoutBreakpoint})`);
+
+	return (
+		<section
+			className={Combine([
+				'standard-component',
+				'section',
+				'hero-section',
+				styles['hero-section'],
+				className,
+			])}
+			data-layout={!isWideLayout ? 'narrow' : 'wide'}
+			{...properties}
+		>
+			<Wrapper>
+				{heading}
+				<div className={styles['hero-children']}>{children}</div>
+			</Wrapper>
+		</section>
+	);
+}
