bdb3df0cf5e13defb64512d221f179c0fa11014f
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-12-02 15:06:01 -0800

    fix: add hero-section export to main.ts (#39)

--- lib/main.ts
+++ lib/main.ts
@@ -2,7 +2,10 @@ export { AnchorButton } from './components/buttons/anchor-button';
 export { ButtonGroup } from './components/buttons/button-group';
 export { RadioButton } from './components/buttons/radio-button';
 export { StandardButton } from './components/buttons/standard-button';
+
 export { Footer } from './components/root/footer';
 export { Header } from './components/root/header';
 export { Page } from './components/root/page';
+
+export { HeroSection } from './components/sections/hero-section';
 export { StandardSection } from './components/sections/standard-section';
