f051351d0e139c48fbd734399c3e1b6be49705ef
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-12-03 16:40:16 -0800

    fix: adding missing navigation export (#50)

--- lib/main.ts
+++ lib/main.ts
@@ -1,11 +1,17 @@
-export { AnchorButton } from './components/buttons/anchor-button';
-export { ButtonGroup } from './components/buttons/button-group';
-export { RadioButton } from './components/buttons/radio-button';
-export { StandardButton } from './components/buttons/standard-button';
-
+// Root Exports
 export { Footer } from './components/root/footer';
 export { Header } from './components/root/header';
 export { Page } from './components/root/page';
 
+// Section Exports
 export { HeroSection } from './components/sections/hero-section';
 export { StandardSection } from './components/sections/standard-section';
+
+// Misc Exports
+export { Navigation } from './components/misc/navigation';
+
+// Button Exports
+export { AnchorButton } from './components/buttons/anchor-button';
+export { ButtonGroup } from './components/buttons/button-group';
+export { RadioButton } from './components/buttons/radio-button';
+export { StandardButton } from './components/buttons/standard-button';
