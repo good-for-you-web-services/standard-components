e790b384b4a032e10be7864ba58731091a8e7ad1
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-02-18 15:18:13 -0800

    feat: updating the meta properties to be optional (#87)

--- lib/components/root/page/index.tsx
+++ lib/components/root/page/index.tsx
@@ -1,16 +1,20 @@
-import type { HTMLAttributes, ReactElement } from 'react';
+import { Fragment, type HTMLAttributes, type ReactElement } from 'react';
 import { Combine } from '../../../functions/combine';
 import { StandardSection } from '../../sections/standard-section';
 import { Wrapper } from '../../wrappers/wrapper';
 import styles from './styles.module.scss';
 
 interface Properties extends HTMLAttributes<HTMLBaseElement> {
-	meta: {
-		sitename: string;
-		title: string;
-		description: string;
-		url: string;
-		image: string;
+	meta?: {
+		sitename?: string;
+		title?: string;
+		description?: string;
+		url?: string;
+		image?: string;
+		other?: {
+			name: string;
+			content: string;
+		}[];
 	};
 	heading?: ReactElement<HTMLHeadingElement>;
 }
@@ -27,14 +31,23 @@ export function StandardPage({
 			className={Combine(['standard-component', 'page', styles['page'], className])}
 			{...otherProperties}
 		>
-			<meta name='title' content={meta.title} />
-			<meta name='description' content={meta.description} />
+			{meta && (
+				<Fragment>
+					{meta.title && <meta name='title' content={meta.title} />}
+					{meta.description && <meta name='description' content={meta.description} />}
 
-			<meta property='og:description' content={meta.description} />
-			<meta property='og:title' content={meta.title} />
-			<meta property='og:url' content={meta.url} />
-			<meta property='og:image' content={meta.image} />
-			<meta property='og:site_name' content={meta.sitename} />
+					{meta.sitename && <meta property='og:site_name' content={meta.sitename} />}
+					{meta.title && <meta property='og:title' content={meta.title} />}
+					{meta.description && (
+						<meta property='og:description' content={meta.description} />
+					)}
+					{meta.url && <meta property='og:url' content={meta.url} />}
+					{meta.image && <meta property='og:image' content={meta.image} />}
+
+					{meta.other &&
+						meta.other.map((item) => <meta name={item.name} content={item.content} />)}
+				</Fragment>
+			)}
 
 			<Wrapper className={styles['wrapper']} fullWidth>
 				{heading && (
