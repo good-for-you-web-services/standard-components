f4b7cd1850b7000f3409c83d6bd2a6dd9faf7fa6
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-02-18 22:40:56 +0000

    feat: updating the page component to support metadata tags

--- lib/components/root/page/index.tsx
+++ lib/components/root/page/index.tsx
@@ -5,17 +5,37 @@ import { Wrapper } from '../../wrappers/wrapper';
 import styles from './styles.module.scss';
 
 interface Properties extends HTMLAttributes<HTMLBaseElement> {
+	meta: {
+		sitename: string;
+		title: string;
+		description: string;
+		url: string;
+		image: string;
+	};
 	heading?: ReactElement<HTMLHeadingElement>;
 }
 
-export function Page(properties: Properties) {
-	const { heading, children, className, ...otherProperties } = properties;
-
+export function StandardPage({
+	meta,
+	heading,
+	className,
+	children,
+	...otherProperties
+}: Properties) {
 	return (
 		<main
 			className={Combine(['standard-component', 'page', styles['page'], className])}
 			{...otherProperties}
 		>
+			<meta name='title' content={meta.title} />
+			<meta name='description' content={meta.description} />
+
+			<meta property='og:description' content={meta.description} />
+			<meta property='og:title' content={meta.title} />
+			<meta property='og:url' content={meta.url} />
+			<meta property='og:image' content={meta.image} />
+			<meta property='og:site_name' content={meta.sitename} />
+
 			<Wrapper className={styles['wrapper']} fullWidth>
 				{heading && (
 					<StandardSection className='heading-section'>{heading}</StandardSection>
