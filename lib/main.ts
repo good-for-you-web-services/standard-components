6b4eec53c0c4447aa639cf214927655eb39fc02b
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-03-07 21:38:28 +0000

    feat: renaming image component folder

--- lib/main.ts
+++ lib/main.ts
@@ -4,4 +4,4 @@ export { StandardHeader } from './components/blocks/standard-header';
 export { StandardPage } from './components/blocks/standard-page';
 export { StandardSection } from './components/blocks/standard-section';
 
-export { StandardImage } from './components/elements/image';
+export { StandardImage } from './components/elements/standard-image';
